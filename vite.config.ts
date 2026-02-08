cd879411562666402bd89e404057867c903df180
Kimdesignui <pt.zenco@gmail.com>
2026-02-08 15:20:17 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, process.cwd(), '');
+    
     return {
+      base: './',
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -16,8 +18,16 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(__dirname, './'),
         }
+      },
+      build: {
+        outDir: 'dist',
+        rollupOptions: {
+          input: {
+            main: path.resolve(__dirname, 'index.html'),
+          },
+        },
       }
     };
 });
