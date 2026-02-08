7ce873c455a6bbb29130b2b0776473543bf242c0
Kimdesignui <pt.zenco@gmail.com>
2026-02-08 15:31:03 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     return {
-      base: './',
+      base: './', // Cực kỳ quan trọng để không mất ảnh
       plugins: [react()],
       define: {
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
