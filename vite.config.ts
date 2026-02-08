7c20cdcb1d2d973165d0b366a704ec42242e48af
Kimdesignui <pt.zenco@gmail.com>
2026-02-08 15:24:28 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
-    
     return {
       base: './',
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
-        alias: {
-          '@': path.resolve(__dirname, './'),
-        }
+        alias: { '@': path.resolve(__dirname, './') }
       },
       build: {
         outDir: 'dist',
         rollupOptions: {
-          input: {
-            main: path.resolve(__dirname, 'index.html'),
-          },
-        },
+          input: { main: path.resolve(__dirname, 'index.html') }
+        }
       }
     };
 });
